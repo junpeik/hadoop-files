e2a6daddf5192ad531343aaef5ac30533b2011e6
Junpei <kusumoto.0513.jp@gmail.com>
2016-02-03 16:11:42 +0900

    map-reduceファイル追加

---
+++ mapper_yahoo.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+require 'natto'
+
+natto = Natto::MeCab.new
+
+ARGF.each_line do |line|
+  natto.parse(line) do |n|
+    if /^名詞/ =~ n.feature.split(',')[0]
+      puts "#{n.surface}\t1"
+    end
+  end
+end
